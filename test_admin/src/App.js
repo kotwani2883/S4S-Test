24839b91ea36b36eff8c0029f81e09ad097f5a96
Palak <palakkotwani2883@gmail.com>
2021-02-24 13:22:53 +0530

    Cors added to server side

--- test_admin/src/App.js
+++ test_admin/src/App.js
@@ -2,7 +2,7 @@ import { Admin,Resource,ListGuesser } from 'react-admin';
 import React from 'react';
 import './App.css';
 import UserIcon from '@material-ui/icons/People';
-import Dashboard from './dashboard'
+import dashboard from './dashboard'
 //import jsonServerProvider from 'ra-data-json-server';
 import dataProvider from './dataProvider'
 //import simpleRestProvider from 'ra-data-simple-rest';
@@ -11,7 +11,7 @@ import dataProvider from './dataProvider'
 const App=()=>{
   return (
    <>
-    <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+    <Admin dashboard={dashboard } dataProvider={dataProvider}>
    
      <Resource name="users" list={ListGuesser} icon={UserIcon}/>
       </Admin>
